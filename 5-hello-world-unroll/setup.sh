bc2a5e4e5f5aa596f25106a44e9d3fc8a98df897
Andre Comodo <comodozin@gmail.com>
2021-05-04 05:51:03 -0300

    Adding first examples

---
+++ 5-hello-world-unroll/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ~/Xilinx/Vitis/2020.2/settings64.sh
+source /opt/petalinux/2020.2/environment-setup-aarch64-xilinx-linux
+export PLATFORM_REPO_PATHS=/opt/xilinx/platforms/xilinx_zcu104_base_202020_1
+export EDGE_COMMON_SW=/home/perina/Downloads/xilinx-zynqmp-common-v2020.2
+export SYSROOT=/opt/petalinux/2020.2/sysroots/aarch64-xilinx-linux
---
+++ 6-hello-world-unroll-noexitcond/project/src/vadd.cpp
@@ -0,0 +1,12 @@
+extern "C" {
+
+void vadd(unsigned int *in1, unsigned int *in2, unsigned int *out, int size) {
+	for(unsigned int i = 0; i < size; i++) {
+#pragma HLS PIPELINE off
+#pragma HLS UNROLL factor=4 skip_exit_check
+#pragma HLS LOOP_TRIPCOUNT max=4096
+		out[i] = in1[i] + in2[i];
+	}
+}
+
+}
