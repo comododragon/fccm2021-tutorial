f3416d1744b98bcc62ab61d470bb761cb275d5f5
Andre Comodo <comodozin@gmail.com>
2021-05-05 06:41:20 -0300

    Implementing example 3; moved some common files to the repo root

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /opt/xilinx/Vitis/2020.2/settings64.sh
+source /opt/xilinx/petalinux/2020.2/environment-setup-aarch64-xilinx-linux
+export PLATFORM_REPO_PATHS=/opt/xilinx/platforms/xilinx_zcu104_base_202020_1
+export EDGE_COMMON_SW=/opt/xilinx/rootfs/xilinx-zynqmp-common-v2020.2
+export SYSROOT=/opt/xilinx/petalinux/2020.2/sysroots/aarch64-xilinx-linux
