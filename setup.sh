c8824e3ef14bd6fbbcc41f1f459bdca99a87c6b5
André Bannwart Perina <andre.perina@xfel.eu>
2025-05-27 14:36:08 +0200

    Updated setup.sh with new installs on XFEL pc

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /data/opt/Xilinx/Vitis/2024.1/settings64.sh
-source /data/$USER/petalinux/2024.1/environment-setup-cortexa72-cortexa53-xilinx-linux
+source /data/opt/Xilinx/petalinux/2024.1/environment-setup-cortexa72-cortexa53-xilinx-linux
 export PLATFORM_REPO_PATHS=/data/opt/Xilinx/Vitis/2024.1/base_platforms/xilinx_zcu104_base_202410_1
-export EDGE_COMMON_SW=/data/$USER/xilinx-zynqmp-common-v2024.1
-export SYSROOT=/data/$USER/petalinux/2024.1/sysroots/cortexa72-cortexa53-xilinx-linux
+export EDGE_COMMON_SW=/data/opt/Xilinx/rootfs/2024.1/xilinx-zynqmp-common-v2024.1
+export SYSROOT=/data/opt/Xilinx/petalinux/2024.1/sysroots/cortexa72-cortexa53-xilinx-linux
